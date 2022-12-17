c30a17a8d59f483ff6b7673755cfd0b4181b560d
CharaD7 <jijakahn6@gmail.com>
2022-12-17 11:01:41 +0000

    Added path rules to import order

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,8 @@ import express from 'express';
 const app = express();
 const port = 9000;
 
-const one: number = 1;
-const two: number = 2;
+const one = 1;
+const two = 2;
 
 app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
 
