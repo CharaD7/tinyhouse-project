2dfc84ac9a767c9ac918b0a8ad5e40dbb34514c5
CharaD7 <jijakahn6@gmail.com>
2022-12-17 19:35:22 +0000

    Added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	arrowParams: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	jsxBracketSameLine: false,
+	jsxSingleQuote: false,
+	quoteProps: 'as-needed',
+	singleQuote: true,
+	semi: true,
+	useTabs: false,
+	tabWidth: 2,
+	trailingComma: 'es6'
+}
