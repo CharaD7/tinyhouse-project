2f4cb23d59c39f8eaf337b28bd7b59bd08d09eb1
CharaD7 <jijakahn6@gmail.com>
2022-12-17 09:42:23 +0000

    Added Eslint

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 9000;
+
+const one: number = 1;
+const two: number = 2;
+
+app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
+
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);
