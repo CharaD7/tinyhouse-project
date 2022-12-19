c843285dd0f56770382e9172f9d53ebac79b85fb
CharaD7 <jijakahn6@gmail.com>
2022-12-19 00:01:49 +0000

    Started delete-listing route

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 
 import { listings } from './listing';
@@ -5,12 +6,18 @@ import { listings } from './listing';
 const app = express();
 const port = 9000;
 
+app.use(bodyParser.json());
+
 const one = 1;
 const two = 2;
 
 app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
 
 app.get('/listings', (req, res) => res.send(listings));
+
+app.get('/delete-listing', (req, res) => {
+	const id = req.body.id;
+});
 
 app.listen(port);
 
