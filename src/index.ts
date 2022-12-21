ccf174ed560e0430e3b9e8da56d8ba2405d2f91f
CharaD7 <jijakahn6@gmail.com>
2022-12-21 14:32:39 +0000

    Created graphql instance for initializing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer();
+const server = new ApolloServer({ schema });
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
