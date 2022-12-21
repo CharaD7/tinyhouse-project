390f191e901876367e0664ec0a34205337dd01a5
CharaD7 <jijakahn6@gmail.com>
2022-12-21 00:28:27 +0000

    Created graphql instance for initializing

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-import bodyParser from 'body-parser';
 import express from 'express';
-
-import { listings } from './listing';
+import { ApolloServer } from 'apollo-server-express';
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-app.get('/listings', (req, res) => res.send(listings));
-
-app.post('/delete-listing', (req, res) => {
-	const id: string = req.body.id;
-
-	for (let i = 0; i < listings.length; i++) { // check for the particular id specified
-		if (listings[i].id === id) { // if the id is found
-			return res.send(listings.splice(i, 1)); // splice deletes the id and returns the message, but we use 1 since we only want to delete one item
-		}
-	}
-
-	return res.send("Failed to delete listing"); // if id does not exist
-});
+const server = new ApolloServer();
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
