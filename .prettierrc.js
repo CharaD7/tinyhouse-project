e1b3e84692245942f07cafcb152c52b153ff3bb7
CharaD7 <jijakahn6@gmail.com>
2023-01-02 09:34:52 +0000

    Added periods in package.json for eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-	arrowParams: 'always',
-	bracketSpacing: true,
-	endOfLine: 'lf',
-	jsxBracketSameLine: false,
-	jsxSingleQuote: false,
-	quoteProps: 'as-needed',
-	printWidth: 80,
-	singleQuote: true,
-	semi: true,
-	useTabs: false,
-	tabWidth: 2,
-	trailingComma: 'es6'
-}
+  arrowParams: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  printWidth: 80,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
